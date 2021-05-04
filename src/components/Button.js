fd103357f84ea11e420ac18b394e205166fc28b0
Sinan <syaman14@ku.edu.tr>
2021-05-04 22:43:42 +0300

    Default button layout with minial variances

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,31 @@
+import PropTypes from 'prop-types';
+import '../assets/button.css';
+
+
 export const Button = props => {
-    return <p>Button</p>
+
+    const {
+        variant,
+        disableShadow,
+        disabled,
+        startIcon,
+        endIcon,
+        size,
+        color,
+        children
+    } = props;
+
+    return <div className={`saynn-button ${variant || ''} ${disableShadow && 'no-shadow'}`}> 
+    {children} 
+    </div>
+}
+
+Button.propTypes = {
+    variant: PropTypes.string,
+    disableShadow: PropTypes.bool,
+    disabled: PropTypes.bool,
+    startIcon: PropTypes.string,
+    endIcon: PropTypes.string,
+    size: PropTypes.string,
+    color: PropTypes.string,
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Button} from './components/Button';
+import { Button } from './components/Button';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Button />
+    <p>Default</p>
+    <Button>Test</Button>
+    <p>Text</p>
+    <Button variant="text">Test</Button>
+    <p>Outline</p>
+    <Button variant="outline">Test</Button>
+    <p>Disable Shadow</p>
+    <Button disableShadow>Test</Button>
   </React.StrictMode>,
   document.getElementById('root')
 );
